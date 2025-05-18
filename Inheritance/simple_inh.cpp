a4f927c5ce0bb47cbe9a17d901c0e603db1fd78e
Hira_Nawaz <nawazhira48@gmail.com>
2025-05-18 14:43:23 +0500

    Create simple_inh.cpp

---
+++ Inheritance/simple_inh.cpp
@@ -0,0 +1,24 @@
+// 2415.
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    void eat() {
+        cout << "Eating..." << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void bark() {
+        cout << "Barking..." << endl;
+    }
+};
+
+int main() {
+    Dog d;
+    d.eat();   // inherited from Animal
+    d.bark();  // defined in Dog
+    return 0;
+}
